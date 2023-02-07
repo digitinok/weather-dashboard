72d852868c7c8930b15183e273b929475de5ec23
digitinok <75326757+digitinok@users.noreply.github.com>
2023-02-07 21:29:14 +0000

    get API key working for current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,62 @@
 let lat  = "51.5";
 let lon = "0";
-let apiKey = "4ddb322a76968b6cb599fa2b021f691b";
-let queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+city = "London";
+let APIKey = "4ddb322a76968b6cb599fa2b021f691b";
+let queryURL2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+
+let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+
+
 
 
 // created an AJAX call
 $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    
-    // Create CODE HERE to Log the queryURL
-    console.log(queryURL);
-    // Create CODE HERE to log the resulting object
-    console.log(response);
-    // Create CODE HERE to calculate the temperature (converted from Kelvin)
-    // Create CODE HERE to transfer content to HTML
-    // Hint: To convert from Kelvin to Celsius: C = K - 273.15
-    // Create CODE HERE to dump the temperature content into HTML
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
   
-    console.log(response.main.temp - 273.15)
-  /*
-    $(".city").html(`<h1>${response.name} Weather Details</h1>`)
-    $(".wind").text(`Wind Speed: ${response.wind.speed}`)
-    $(".humidity").text(`Humidity: ${response.main.humidity}`)
-    $(".temp").text(`Temperature [degC]: ${(response.main.temp - 273.15).toFixed(1)}`)
-  */
+  // Create CODE HERE to Log the queryURL
+  console.log(queryURL);
+  // Create CODE HERE to log the resulting object
+  console.log(response);
+  // Create CODE HERE to calculate the temperature (converted from Kelvin)
+  // Create CODE HERE to transfer content to HTML
+  // Hint: To convert from Kelvin to Celsius: C = K - 273.15
+  // Create CODE HERE to dump the temperature content into HTML
 
-  });
+  console.log(response.main.temp - 273.15)
+/*
+  (".city").html(`<h1>${response.name} Weather Details</h1>`)
+  $(".wind").text(`Wind Speed: ${response.wind.speed}`)
+  $(".humidity").text(`Humidity: ${response.main.humidity}`)
+  $(".temp").text(`Temperature [degC]: ${(response.main.temp - 273.15).toFixed(1)}`)
+*/
+
+});
+
+
+// created an AJAX call
+$.ajax({
+  url: queryURL2,
+  method: "GET"
+}).then(function(response) {
   
+  // Create CODE HERE to Log the queryURL
+  console.log(queryURL2);
+  // Create CODE HERE to log the resulting object
+  console.log(response);
+  // Create CODE HERE to calculate the temperature (converted from Kelvin)
+  // Create CODE HERE to transfer content to HTML
+  // Hint: To convert from Kelvin to Celsius: C = K - 273.15
+  // Create CODE HERE to dump the temperature content into HTML
+
+  console.log(response.main.temp - 273.15)
+/*
+  $(".city").html(`<h1>${response.name} Weather Details</h1>`)
+  $(".wind").text(`Wind Speed: ${response.wind.speed}`)
+  $(".humidity").text(`Humidity: ${response.main.humidity}`)
+  $(".temp").text(`Temperature [degC]: ${(response.main.temp - 273.15).toFixed(1)}`)
+*/
+
+});
+
