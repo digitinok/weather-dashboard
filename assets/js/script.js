019421f101721af6a7f78f806e0bb125e21806f2
digitinok <75326757+digitinok@users.noreply.github.com>
2023-02-07 23:54:04 +0000

    get weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,48 @@
 let lat  = "51.5";
 let lon = "0";
-city = "London";
+//let city = "London";
 let APIKey = "4ddb322a76968b6cb599fa2b021f691b";
 let queryURL2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
-let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+//let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
 
-// initialise cities arry, get cities from local storage
+let historyListEl = $("#history-list");
+
+// display all the city search history as buttons
+let renderCitySearchHistory = () => {
+  const cities = JSON.parse(localStorage.getItem('cities'));
+  for (let i=0; i<cities.length; i++) {
+    let cityEl = $("<li>").text(cities[i]);
+    // display weather data
+    cityEl.on("click", console.log(cities[i]))
+    historyListEl.append(cityEl);
+  }
+}
+
+// display the currentWeather
+let currentWeather = (city) => {
+  let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+  console.log(city);
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+
+    console.log(response);
+    // temperature (converted from Kelvin)
+    console.log(response.main.temp - 273.15)
+  /*
+    (".city").html(`<h1>${response.name} Weather Details</h1>`)
+    $(".wind").text(`Wind Speed: ${response.wind.speed}`)
+    $(".humidity").text(`Humidity: ${response.main.humidity}`)
+    $(".temp").text(`Temperature [degC]: ${(response.main.temp - 273.15).toFixed(1)}`)
+  */
+  
+  });
+}
+
+
+// initialise cities array, get cities from local storage
 let cities = JSON.parse(localStorage.getItem('cities'));
 console.log(cities);
 if (cities === null) {cities = []}
@@ -15,29 +52,20 @@ let searchButton = $("#search-button")
 
 searchButton.on("click", (event) => {
   event.preventDefault();
-  city = $("#search-input").val().toLowerCase().trim();
+  let city = $("#search-input").val().toLowerCase().trim();
   $("#search-input").val("");
 
-  // searched sities saved to local starage and buttons rendered
+  // searched cities saved to local starage and buttons rendered
   if (city !== "" && !cities.includes(city)) {
     cities.push(city);
     localStorage.setItem("cities", JSON.stringify(cities));
     renderCitySearchHistory();
   }
-  console.log(cities);
+  currentWeather(city);
 });
 
-let historyListEl = $("#history-list");
 
-// display all the city search history as buttons
-let renderCitySearchHistory = () => {
-  const cities = JSON.parse(localStorage.getItem('cities'));
-  for (let i=0; i<cities.length; i++) {
-    let cityEl = $("<li>").text(cities[i]);
-    historyListEl.append(cityEl);
-  }
-}
-
+// display search history of all previously searched cities
 renderCitySearchHistory();
 
 /*
