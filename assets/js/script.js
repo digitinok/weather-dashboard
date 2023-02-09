08e5c8af2eb35e553b02f9ad382528a3b3d4c0e5
digitinok <75326757+digitinok@users.noreply.github.com>
2023-02-09 01:48:13 +0000

    displaying curren weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,65 @@
 let lat  = "51.5";
 let lon = "0";
-//let city = "London";
-let APIKey = "4ddb322a76968b6cb599fa2b021f691b";
-let queryURL2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
-//let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-
+let queryType = "weather";  //forecast
+const APIKey = "4ddb322a76968b6cb599fa2b021f691b";
+//let queryURL = `https://api.openweathermap.org/data/2.5/${queryType}?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+//let queryURL2 = `http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit=1&appid=${APIKey}`;
 let historyListEl = $("#history-list");
+
+
+let getWeather = (event) => {
+  let city = event.target.dataset.city;
+  console.log(city);
+  getCurrentWeather("weather", lat, lon, APIKey);
+}
+
 
 // display all the city search history as buttons
 let renderCitySearchHistory = () => {
   const cities = JSON.parse(localStorage.getItem('cities'));
   for (let i=0; i<cities.length; i++) {
-    let cityEl = $("<li>").text(cities[i]);
+    let itemEl = $("<li>");
+    let cityEl = $("<button>").text(cities[i]);
+    cityEl.attr("data-city", cities[i]);
     // display weather data
-    cityEl.on("click", console.log(cities[i]))
-    historyListEl.append(cityEl);
+    cityEl.on("click", getWeather);
+    itemEl.append(cityEl);
+    historyListEl.append(itemEl);
   }
 }
 
-// display the currentWeather
-let currentWeather = (city) => {
-  let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-  console.log(city);
 
+
+
+// display the currentWeather
+let getCurrentWeather = (queryType="weather", lat, lon, APIKey) => {
+  let queryURL = `https://api.openweathermap.org/data/2.5/${queryType}?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+  console.log(queryURL)
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-
-    console.log(response);
-    // temperature (converted from Kelvin)
-    console.log(response.main.temp - 273.15)
-  /*
-    (".city").html(`<h1>${response.name} Weather Details</h1>`)
-    $(".wind").text(`Wind Speed: ${response.wind.speed}`)
-    $(".humidity").text(`Humidity: ${response.main.humidity}`)
-    $(".temp").text(`Temperature [degC]: ${(response.main.temp - 273.15).toFixed(1)}`)
-  */
+    console.log(response)
   
+    // city name  
+    let city = $("#city").text(`${response.name} `);
+    // weather Image in City name header
+    let weatherImg = $("<img>");
+    weatherImg.attr("src", `http://openweathermap.org/img/wn/${(response.weather[0].icon)}.png`)
+    weatherImg.attr("alt", "weather icon");
+    city.append(weatherImg);
+
+    // date
+    $("#date").text(`${moment(response.dt*1000 + response.timezone*1000).format('LLLL')}`);
+    // icon representation of weather conditions  
+    $("#weather").text(`${response.weather[0].description}`);
+    // temperature
+    $("#temp").text(`Temperature: ${(response.main.temp - 273.15).toFixed(1)} °C`);
+    // humidity
+    $("#humidity").text(`Humidity: ${response.main.humidity} %`);
+    // wind speed
+    $("#wind").text(`Wind Speed: ${response.wind.speed} m/s`);
+   
   });
 }
 
@@ -47,76 +69,51 @@ let cities = JSON.parse(localStorage.getItem('cities'));
 console.log(cities);
 if (cities === null) {cities = []}
 
-//get city from search input with the submit button
-let searchButton = $("#search-button")
 
+// get city from search input with the submit button
+let searchButton = $("#search-button")
 searchButton.on("click", (event) => {
   event.preventDefault();
   let city = $("#search-input").val().toLowerCase().trim();
   $("#search-input").val("");
 
-  // searched cities saved to local starage and buttons rendered
+  // searched cities saved to local storage and buttons rendered
   if (city !== "" && !cities.includes(city)) {
+    let queryURL2 = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIKey}`;
+    
+      // created an AJAX call
+    $.ajax({
+      url: queryURL2,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response)
+
+    });
+
     cities.push(city);
     localStorage.setItem("cities", JSON.stringify(cities));
     renderCitySearchHistory();
   }
-  currentWeather(city);
+  getCurrentWeather("weather", lat, lon, APIKey);
 });
 
 
 // display search history of all previously searched cities
 renderCitySearchHistory();
 
-/*
-// created an AJAX call
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  
-  // Create CODE HERE to Log the queryURL
-  console.log(queryURL);
-  // Create CODE HERE to log the resulting object
-  console.log(response);
-  // Create CODE HERE to calculate the temperature (converted from Kelvin)
-  // Create CODE HERE to transfer content to HTML
-  // Hint: To convert from Kelvin to Celsius: C = K - 273.15
-  // Create CODE HERE to dump the temperature content into HTML
 
-  console.log(response.main.temp - 273.15)
-/*
-  (".city").html(`<h1>${response.name} Weather Details</h1>`)
-  $(".wind").text(`Wind Speed: ${response.wind.speed}`)
-  $(".humidity").text(`Humidity: ${response.main.humidity}`)
-  $(".temp").text(`Temperature [degC]: ${(response.main.temp - 273.15).toFixed(1)}`)
+// display the 5 day Weather Forcast
+let getCForcastWeather = (queryType="forecast", lat, lon, APIKey) => {
+  let queryURL = `https://api.openweathermap.org/data/2.5/${queryType}?lat=${lat}&lon=${lon}&appid=${APIKey}`;
 
+  // created an AJAX call for 5 day weather forecast
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {``
+    console.log(response)
 
-});*/
+});
+}
 
-/*
-// created an AJAX call
-$.ajax({
-  url: queryURL2,
-  method: "GET"
-}).then(function(response) {
-  
-  // Create CODE HERE to Log the queryURL
-  console.log(queryURL2);
-  // Create CODE HERE to log the resulting object
-  console.log(response);
-  // Create CODE HERE to calculate the temperature (converted from Kelvin)
-  // Create CODE HERE to transfer content to HTML
-  // Hint: To convert from Kelvin to Celsius: C = K - 273.15
-  // Create CODE HERE to dump the temperature content into HTML
-
-  console.log(response.main.temp - 273.15)
-/*
-  $(".city").html(`<h1>${response.name} Weather Details</h1>`)
-  $(".wind").text(`Wind Speed: ${response.wind.speed}`)
-  $(".humidity").text(`Humidity: ${response.main.humidity}`)
-  $(".temp").text(`Temperature [degC]: ${(response.main.temp - 273.15).toFixed(1)}`)
-
-
-});*/
-
+getCForcastWeather = ("forecast", lat, lon, APIKey);
