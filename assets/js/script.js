23d9b26d2f988d01172f1d121ff4f6e33bcfb840
digitinok <75326757+digitinok@users.noreply.github.com>
2023-02-12 17:56:12 +0000

    5-day forcast displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,7 @@ let getWeather = (event) => {
 
   console.log(event.target.dataset.city);
   // city name  
-  let city = $("#city").text(`${event.target.dataset.city}`);
-
+  $("#city").text(`${event.target.dataset.city}`);
   getForcastWeather("forecast", event.target.dataset.lat, event.target.dataset.lon, APIKey);
   getCurrentWeather("weather", event.target.dataset.lat, event.target.dataset.lon, APIKey);
  }
@@ -47,19 +46,34 @@ console.log("Hello")
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response)
 
-    
+    // find next lunchtime to display data
     let startTime = 5;
-    // weather Image and desription
-    const weather = `<img src="http://openweathermap.org/img/wn/${(response.list[startTime].weather[0].icon)}.png"/> ${response.list[startTime].weather[0].description}`;
+    let nextTime = startTime;
 
-    // display date, weather conditions, temperature, humidity, wind speed
-    $("#dateF1").text(`${moment(response.list[startTime].dt*1000 + response.city.timezone*1000).format('dddd, DD-MM-YY, HH:ss')}`);
-    $("#weatherF1").html(weather);
-    $("#tempF1").text(`Temperature: ${(response.list[startTime].main.temp - 273.15).toFixed(1)} °C`); 
-    $("#humidityF1").text(`Humidity: ${response.list[startTime].main.humidity} %`);
-    $("#windF1").text(`Wind: ${(response.list[startTime].wind.speed * 3.6).toFixed(1)} km/h`);
+    //5 day weather forcast
+    for (let i=0; i<5; i++) {
+      
+      if (i!==0){nextTime+=8;} // 8 time steps for 24 hours
+      let dateEl = $("<h5></h5>");
+      let weatherImg = $("<p></p>");
+      let weatherEl = $("<p></p>");
+      let tempEl = $("<p></p>");
+      let humidityEl = $("<p></p>");
+      let windEl = $("<p></p>");
+
+      // display date, weather conditions, temperature, humidity, wind speed
+      dateEl.text(`${moment(response.list[nextTime].dt*1000 + response.city.timezone*1000).format('ddd, DD.MM.YY')}`);
+      weatherImg.html(`<img src="http://openweathermap.org/img/wn/${(response.list[nextTime].weather[0].icon)}.png"/>`);
+      weatherEl.text(`${response.list[nextTime].weather[0].description}`);
+      tempEl.text(`Temp: ${(response.list[nextTime].main.temp - 273.15).toFixed(1)} °C`); 
+      humidityEl.text(`Humidity: ${response.list[nextTime].main.humidity} %`);
+      windEl.text(`Wind: ${(response.list[nextTime].wind.speed * 3.6).toFixed(1)} km/h`);
+      //append data to corresponding card
+      $("#"+i.toString()).html("");
+      $("#"+i.toString()).append(dateEl, weatherImg, weatherEl, tempEl, humidityEl, windEl);
+
+    }
 
 });
 }
@@ -72,20 +86,17 @@ let getCurrentWeather = (queryType="weather", lat, lon, APIKey) => {
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-
-    // weather Image and desription
-    const weather = `<img src="http://openweathermap.org/img/wn/${(response.weather[0].icon)}.png"/> ${response.weather[0].description}`;
-
+    
     // display date, weather conditions, temperature, humidity, wind speed
-    $("#date").text(`${moment(response.dt*1000 + response.timezone*1000).format('dddd, DD-MM-YY, HH:ss')}`);
-    $("#weather").html(weather);
+    $("#date").text(`${moment(response.dt*1000 + response.timezone*1000).format('dddd, DD.MM.YYYY, HH:ss')}`);
+    $("#weather-img").html(`<img src="http://openweathermap.org/img/wn/${(response.weather[0].icon)}.png"/>`);
+    $("#weather").text(`${response.weather[0].description}`);
     $("#temp").text(`Temperature: ${(response.main.temp - 273.15).toFixed(1)} °C`); 
     $("#humidity").text(`Humidity: ${response.main.humidity} %`);
     $("#wind").text(`Wind: ${(response.wind.speed * 3.6).toFixed(1)} km/h`);
    
   }); 
 }
-
 
 
 // wait until the page has loaded
@@ -103,30 +114,34 @@ $(window).on('load', () => {
     let city = $("#search-input").val().toLowerCase().trim();
     $("#search-input").val("");
 
-    // searched cities saved to local storage and buttons rendered
+    // check if city is already in list
     if (city === "") {return}
     for (let i=0; i<cities.length; i++) {
-      console.log(cities[i].name.toLowerCase());
-      console.log(city);
+
       if (cities[i].name.toLowerCase() === city) {
-        // display weather and return
-        // getCurrentWeather("weather", response.lat, response.lon, APIKey);
+        // display city name, weather, forcast and return
+        let data = $("#history-list").children()[i].dataset;
+        $("#city").text(`${data.city}`);
+        getCurrentWeather("weather", data.lat, data.lon, APIKey);
+        getForcastWeather("forecast", data.lat, data.lon, APIKey);
         return
       } 
     } 
-    // get longitude and latitude for the place and 
-          // created an AJAX call for location data 
+    // get longitude and latitude for the new city 
+    // created an AJAX call for location data 
     $.ajax({
       url: `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIKey}`,
       method: "GET"
     }).then(function(response) {
       /* country, lat, local_names, lon, name, state */
-      console.log(response)
       if (response.length >   0) {
+        console.log(response)
+        // searched cities saved to local storage and buttons rendered
         cities.push(response[0]); 
         localStorage.setItem("cities", JSON.stringify(cities));
         renderCitySearchHistory();
-        getForcastWeather = ("forecast", response[0].lat, response[0].lon, APIKey);
+        $("#city").text(`${response[0].name} ${response[0].country}`);
+        getForcastWeather("forecast", response[0].lat, response[0].lon, APIKey);
         getCurrentWeather("weather", response[0].lat, response[0].lon, APIKey);
       } else {
         alert(`Sorry, I could not find the place you are looking for!`);
@@ -134,8 +149,6 @@ $(window).on('load', () => {
     });
   });
     
-
-
   // display search history of all previously searched cities
   renderCitySearchHistory();
 
